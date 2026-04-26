cb8a3cc284cc5d1ac8178cc615fdf32510460d85
LucasPro <lucassantosarviedo89@gmail.com>
2026-04-26 11:45:05 -0500

    Preparando deploy en Render

--- index.js
+++ index.js
@@ -76,6 +76,8 @@ app.delete('/productos/:id', (req, res) => {
 });
 
 //Servidor
-app.listen(3000, () => {
-    console.log('Servidor en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor en puerto ${PORT}`);
 });
